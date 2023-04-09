04b9529bfbfc8d9f5cd121827053dc20d4a98417
Jhony Montero <103481905+Jhon2899@users.noreply.github.com>
2023-04-09 06:30:15 -0500

    Add files via upload

---
+++ Operaciones.py
@@ -0,0 +1,6 @@
+#Realizamos operaciones 
+#Asignamos variables
+
+num1= int(3)
+num2=int (4)
+print(num1+num2)
---
+++ Variables.py
@@ -0,0 +1,8 @@
+nombre = "Curso de Python"
+print(nombre)
+
+edad= 28
+print(edad)
+
+saldo = 4.5
+print(saldo)
